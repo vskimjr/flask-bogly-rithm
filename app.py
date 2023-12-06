e2025dc089052144478c29f2ac6244c2c07be788
Victor Kim <vskimjr@gmail.com>
2023-12-06 18:46:35 -0500

    Added guidelines for routes, created templates: base.html, index.html

--- app.py
+++ app.py
@@ -6,9 +6,66 @@ from flask import Flask
 from models import connect_db
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+# Routes
+
+@app.get("/")
+def something():
+    """Redirect to list of users"""
+
+    return render_template("index.html")
+
+
+
+@app.get("/users/")
+def something():
+    """
+    Show all users
+    All users will have a link with their name on it
+    Has link to the add-user form
+    """
+
+@app.get("/users/new")
+def something():
+    """
+    Shows add form for users
+    """
+
+@app.post("/users/new")
+def something():
+    """
+    process the add user form, adding a new user and redirects to /users
+    """
+
+@app.get("/users/int:<user_id>/")
+def something():
+    """
+    Shows information about the given user
+    Has an edit button that redirects to the edit page
+    Has a delete button to delete the user
+
+    """
+
+@app.post("/users/int:<user_id>/edit")
+def something():
+    """
+    show the edit page for a user
+    Have a cancel button that returns to details page for a user
+    Have a save button that updates the user
+
+    """
+
+
+@app.post("/users/int:<user_id>/delete")
+def something():
+    """
+    Delete the user
+
+    """
--- models.py
+++ models.py
@@ -1,1 +1,33 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Model definitions
+
+class User(db.Model):
+    """User for Blogly"""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    first_name = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.Text,
+        nullable=True
+    )
